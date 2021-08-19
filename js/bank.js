fa867eae4408c52e191bee6930326fe93e03aa78
nayemshaon34 <nayemshaon34@gmail.com>
2021-08-20 01:53:11 +0600

    mail condition error update

--- js/bank.js
+++ js/bank.js
@@ -1,7 +1,7 @@
 document.getElementById('info-btn').addEventListener('click',function(){
     const email = document.getElementById('mail-info');
     const pass = document.getElementById('pass-info');
-    if(email.value = 'mail' && pass.value == 'secret'){
+    if(email.value == 'mail' && pass.value == 'secret'){
         
         window.location.href = "banking.html";
         
